e0696c2cf6acced7f5df90449e0725809fa3b012
campkarina <campkarina@yahoo.com>
2016-09-02 11:11:41 -0700

    Added Files

---
+++ APLesson_01/asC.java
@@ -0,0 +1,13 @@
+public class asC
+{
+	public static void main(String[]args)
+	{
+		System.out.println("oooooooooooooooooooooooooo");
+		System.out.println("o\t                                  o");
+		System.out.println("o\t   X      X\t                  o");
+		System.out.println("o\t                                  o");
+		System.out.println("o\t ________\t  o");
+		System.out.println("o\t                                  o");
+		System.out.println("oooooooooooooooooooooooooo");
+	}
+}
---
+++ APLesson_01/asC2.java
@@ -0,0 +1,27 @@
+public class asC2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^OOOO^^^^^^^^^^^^OOOO^^^^^^^^^");
+		System.out.println("^^^^^^^^^^OOOO^^^^^^^^^^^^OOOO^^^^^^^^^");
+		System.out.println("^^^^^^^^^^OOOO^^^^^^^^^^^^OOOO^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^OO^^^^^^^^^OO^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^OO^^^^^^^OO^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^OOO^^^OOO^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^OOOOOOO^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		
+	}
+}
